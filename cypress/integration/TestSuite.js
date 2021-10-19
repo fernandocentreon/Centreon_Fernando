43cf1c335ac9ac4629f0395dd6cd863b4fe4f626
Fernando Marreros <fernando.pncvfs@gmail.com>
2021-10-19 09:20:38 +0200

    file

--- cypress/integration/TestSuite.js
+++ cypress/integration/TestSuite.js
@@ -2,5 +2,11 @@ describe('Sauvegarder Filters', () => {
     it('1. Testcase #1', () => {
       expect(true).to.equal(false)
     })
-  })
+  
+    it('1. Testcase #1', () => {
+        expect(true).to.equal(false)
+      })
+
+
+})
   
